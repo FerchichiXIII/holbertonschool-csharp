dc7a3db49997f0c888fbe8e07fee63820324cf95
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-01 01:03:57 +0100

    Print specifiers

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine ("Percent: {0:00.00}%", percent * 100);
-        Console.WriteLine ("Currency: ${0:00,000.00}", currency);
+		Console.WriteLine ("Percent: "+ percent.ToString("0.00%"));
+        Console.WriteLine ("Currency: "+ currency.ToString("C"));
     }
 }
