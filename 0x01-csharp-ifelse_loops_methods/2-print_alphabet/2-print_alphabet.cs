46ad17e48714ce249ace58d7d249abfd062d0713
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-01 21:43:24 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char n = 'a';
+            while (n <= 'z')
+            {
+                Console.Write(n);
+                n ++;
+            }
+        }
+    }
+}
