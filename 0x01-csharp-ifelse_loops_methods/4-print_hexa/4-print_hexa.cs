825c0210cebc47984ceea13bb9817dcd5cf3a874
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-02 16:05:44 +0100

    Hexadecimal printing

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n < 99)
+            {
+                Console.WriteLine("{0} = 0x{0:x}",n);
+                n++;
+            }
+        }
+    }
+}
