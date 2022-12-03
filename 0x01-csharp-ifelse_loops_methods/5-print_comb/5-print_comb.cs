8c0f75a9f93ec54e7deae7122c1055ebd3535a7e
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-03 14:01:02 +0100

    00...99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n <= 99)
+                {
+                    if (n != 99)
+                    {
+                        Console.Write ("{0:00}, ",n);
+
+                    }
+                    else
+                    {
+                        Console.Write ("{0:00}",n);
+                    }
+                    n++;
+                }
+        }
+    }
+}
