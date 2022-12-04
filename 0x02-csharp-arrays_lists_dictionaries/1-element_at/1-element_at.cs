19e2dea017a453a86af03842a76082a932cedda5
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-04 21:13:02 +0100

    Access an element in an array

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -5,12 +5,11 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        if(index < 0 || index < array.Length -1)
+        if(index < 0 || index > array.Length -1)
         {
             Console.WriteLine("Index out of range");
             return -1; 
         }
-
         return array[index];
     }
 }
