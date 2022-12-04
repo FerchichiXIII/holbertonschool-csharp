db64626ba068c66dce07b4058f76062505e520ef
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-04 11:48:17 +0100

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            int r ,num;
+            if (length <= 0)
+            {
+                Console.Write("\n");
+            }
+            else
+            {
+                for (r = 1; r <= length;r++)
+                {
+                    if(r > 1)
+                    {
+                        for (num = 1; num <= r -1; num ++)
+                        {
+
+                            Console.Write(' ');
+                        }
+                    }
+                    Console.WriteLine("\\");
+                }
+            }
+        }
+    }
