a2d6ea3e183546b87011f84d4d7e5dcc998ad71c
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-15 17:10:47 +0100

    Healthy competition

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -1,11 +1,8 @@
-﻿using System;
-
+﻿
 namespace Enemies
 {
     public class Zombie
-    /// <summary>
-    /// class zomby
-    /// </summary>
+    
     {
         public int health;
         public Zombie()
