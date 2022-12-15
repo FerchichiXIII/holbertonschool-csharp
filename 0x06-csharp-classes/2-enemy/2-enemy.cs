427cb45bbffafadf362e8a00ca8d7bb8001eefba
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-15 23:07:25 +0100

    Health validation

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -18,8 +18,8 @@ namespace Enemies
         }
         public Zombie(int value)
         {
-            /// <summary> public Zombie </summary>
-            if (value <= 0)
+
+            if (value < 0)
             {
                 throw new ArgumentException ("Health must be greater than or equal to 0");
             }
