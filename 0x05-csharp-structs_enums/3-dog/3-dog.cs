fd8e3382d217870616f0479df88bd7069d4a4482
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-18 00:48:19 +0100

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+enum Rating
+{
+    Good, Great, Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog (string _name, float _age, string _owner, Rating _rating)
+    {
+        name = _name;
+        age = _age;
+        owner = _owner;
+        rating = _rating;
+    }
+    public override string ToString()
+    {
+        return string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRatng: {3}", name, age, owner, rating);
+    }
+}
