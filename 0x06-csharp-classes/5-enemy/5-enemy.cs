022bd4f3924c8c629f85341f1e2ebdcdaa181a9a
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-17 01:03:23 +0100

    printing a zombie

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// namespace : Enemies
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Class : Zombie
+        /// </summary>
+        public int health;
+        /// <summary> int health </summary>
+        private string name = "(No name)";
+        /// <summary>prv str name</summary>
+        public string Name
+        {
+            get 
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
+        }
+        /// <summary>Get and Set</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> new instance </summary>
+        public Zombie(int value)
+        {
+
+            if (value < 0)
+            {
+                throw new ArgumentException ("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <summary> that returns the value of health of the Zombie object </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+        ///<summary>public .toString() override that prints the Zombie object’s attributes to stdout</summary>
+        public override string ToString()
+        {
+            return ($"Zombie Name: {name} / Total Health: {health}");
+        }
+    }
+}
