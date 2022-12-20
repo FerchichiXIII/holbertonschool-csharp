e8854739072a2a24c0539a3c0b80097303c4ebb2
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-20 12:29:37 +0100

    This is one of these

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 
-class Obj
-{
-public static bool IsOfTypeInt(object obj)
-{
-if (obj is int)
-{
-return true;
-}
-else
-{
-return false;
-}
-}
-}
+
+    class Obj
+    {
+        public static bool IsOfTypeInt(object obj)
+        {
+            /// <summary> 
+            /// returns True if the object is an int, otherwise return False.
+            /// </summary>
+            if (obj is int)
+            {
+                /// <summary> if obj is int return true </summary>
+                return true;
+            }
+            else
+            {
+                /// <summary> if obj is not int return false </summary>
+                return false;
+            }
+        }
+    }
