24beb3aeb94f07cf7d96acdc59220108685029b9
FerchichiXIII <anasferchichi22@gmail.com>
2023-03-03 17:45:07 +0100

    Rectangle

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>class Shape</summary>
+class Shape
+{
+    ///<summary>Throws an NotImplementedException</summary>
+    public virtual int Area()
+    {
+       throw new NotImplementedException("Area() is not implemented");
+    }
+}
+///<summary>Class Rectangle</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    ///<summary>with set and get</summary>
+    public int width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+    
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+}
