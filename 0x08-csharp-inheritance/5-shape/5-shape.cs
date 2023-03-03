27230e9ca75851dd520a75cda829e9697ff0b323
FerchichiXIII <anasferchichi22@gmail.com>
2023-03-03 15:33:38 +0100

    fix xml

--- 0x08-csharp-inheritance/5-shape/5-shape.cs
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -8,6 +8,7 @@ public class Shape
 {
     public virtual int Area()
     {
+        ///<summary>Throws an NotImplementedException</summary>
         throw new NotImplementedException("Area() is not implemented");
     }
 }
