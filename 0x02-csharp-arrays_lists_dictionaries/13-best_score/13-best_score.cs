48296d502c91040f939d579a882a13ee82a41e88
FerchichiXIII <anasferchichi22@gmail.com>
2023-03-04 18:37:04 +0100

    Best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string bestStudent = "None";
+        int bestScore = int.MinValue;
+
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            if (pair.Value > bestScore)
+            {
+                bestScore = pair.Value;
+                bestStudent = pair.Key;
+            }
+        }
+
+        return bestStudent;
+    }
+}
