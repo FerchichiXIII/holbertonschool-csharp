b2df2d21db07b1eb866e93281728020c2a68ace2
FerchichiXIII <anasferchichi22@gmail.com>
2023-03-05 11:20:02 +0100

    --fix

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,31 +1,27 @@
 ﻿using System;
 
-namespace _14_rectangular_array
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[,] array = new int[5, 5];
+        for (int i = 0; i < 5; i++)
         {
-            int[,] array = new int[5, 5];
-
-            for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
             {
-                for (int j = 0; j < 5; j++)
+                if (i == 2 && j == 2)
                 {
-                    if (i == 2 && j == 2)
-                    {
-                        array[i, j] = 1;
-                    }
-                    else
-                    {
-                        array[i, j] = 0;
-                    }
-
-                    Console.Write(array[i, j] + " ");
+                    array[i, j] = 1;
                 }
-
-                Console.WriteLine();
+                else
+                {
+                    array[i, j] = 0;
+                }
+                Console.Write(array[i, j] + " ");
             }
+            Console.WriteLine();
         }
     }
 }
+
