b4ae65e96b61e187440a12b8b53f3c32118fa226
FerchichiXIII <anasferchichi22@gmail.com>
2023-03-16 13:35:47 +0100

    Dequeue

--- 0x0A-csharp-generics/2-dequeue/queue.cs
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -2,42 +2,27 @@
 
 class Queue<T>
 {
-    /// <summary>
-    /// Queue Class
-    /// </summary>
     int count;
 
     public Node head
     {
-        /// <summary>
-        /// Node head
-        /// </summary>
         get;
         set;
     }
 
     public Node tail
     {
-        ///<summary>
-        ///node tail
-        /// </summary>
         get;
         set;
     }
 
     public string CheckType()
     {
-        /// <summary>
-        /// check the type
-        /// </summary>
         return (typeof(T).ToString());
     }
 
     public void Enqueue(T value)
     {
-        /// <summary>
-        /// Enqueue
-        /// </summary>
         Node newNode = new Node(value);
         if (this.tail == null)
         {
@@ -54,9 +39,6 @@ class Queue<T>
     }
     public T Dequeue()
     {
-        /// <summary>
-        /// method Dequeue
-        /// </summary>
         if (this.head == null)
         {
             Console.WriteLine("Queue is empty");
@@ -71,9 +53,6 @@ class Queue<T>
 
     public int Count()
     {
-        /// <summary>
-        /// count
-        /// </summary>
         return this.count;
     }
     
@@ -81,17 +60,11 @@ class Queue<T>
 
     public class Node
     {
-        /// <summary>
-        /// class Node
-        /// </summary>
         T value;
         Node next = null;
 
         public T Value
         {
-            /// <summary>
-            /// value of T
-            /// </summary>
             get => this.value;
             set
             {
@@ -100,9 +73,6 @@ class Queue<T>
         }
         public Node Next
         {
-            /// <summary>
-            /// node next
-            /// </summary>
             get => this.next;
             set
             {
@@ -112,9 +82,6 @@ class Queue<T>
 
         public Node(T value)
         {
-            /// <summery>
-            /// node of T value
-            /// </summery>
             this.value = value;
             this.next = null;
         }
