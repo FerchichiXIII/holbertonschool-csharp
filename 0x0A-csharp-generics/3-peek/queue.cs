42a46a0f5a184235a79d0600c9be9c62fdf476eb
FerchichiXIII <anasferchichi22@gmail.com>
2023-03-16 14:43:39 +0100

    Peek

---
+++ 0x0A-csharp-generics/3-peek/queue.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+class Queue<T>
+{
+    int count;
+
+    public Node head
+    {
+        get;
+        set;
+    }
+
+    public Node tail
+    {
+        get;
+        set;
+    }
+
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (this.tail == null)
+        {
+            this.head = this.tail = newNode;
+            count += 1;
+        }
+        else
+        {
+            this.tail.Next = newNode;
+            this.tail = newNode;
+            count += 1;
+        }
+
+    }
+    public T Dequeue()
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        Node n= this.head;
+        this.head = this.head.Next;
+        count -= 1;
+        return n.Value;
+
+    }
+    public T Peek()
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return this.head.Value;
+    }
+
+    public int Count()
+    {
+        return this.count;
+    }
+    
+
+
+    public class Node
+    {
+        T value;
+        Node next = null;
+
+        public T Value
+        {
+            get => this.value;
+            set
+            {
+                this.value = value;
+            }
+        }
+        public Node Next
+        {
+            get => this.next;
+            set
+            {
+                this.next = value;
+            }
+        }
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
