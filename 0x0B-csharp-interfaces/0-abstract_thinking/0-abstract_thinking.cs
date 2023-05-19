0dfabf8e8c3a8eac5b8d686333bfcb2b546c6595
FerchichiXIII <anasferchichi22@gmail.com>
2023-05-19 21:05:51 +0100

    Abstract thinking

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+abstract class Base
+{
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Name} is a {this.GetType().Name}";
+    }
+}
