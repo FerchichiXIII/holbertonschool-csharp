6fbf2e82c15866ba5238880942c06b7370b68a16
FerchichiXIII <anasferchichi22@gmail.com>
2023-05-20 12:48:30 +0100

    User interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+/// <summary>
+/// Class Base
+/// </summary>
+
+abstract class Base
+{
+    /// <summary>
+    /// The name of the object.
+    /// </summary>
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType()}";
+    }
+}
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    
+    int durability
+    {
+        get;
+        set; 
+    }
+    void Break();
+}
+
+public interface ICollectable
+{
+    
+
+    bool isCollected
+    {
+        get;
+        set;
+    }
+
+    void Collect();
+
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability
+    {
+        get;
+        set;
+    }
+
+    public bool isCollected
+    {
+        get;
+        set;
+    }
+
+    public void Interact()
+    {
+
+    }
+    public void Break()
+    {
+
+    }
+    public void collect()
+    { 
+    
+    }
+}
