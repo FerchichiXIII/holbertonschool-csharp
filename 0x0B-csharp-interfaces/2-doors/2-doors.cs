2bbf579b924a414e8fdf5b9b54e5d9c8fe89b951
FerchichiXIII <anasferchichi22@gmail.com>
2023-05-22 00:54:32 +0100

    Escape room

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,72 @@
+﻿using System;
+/// <summary>
+/// interface IInteractive
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// interact
+    /// </summary>
+    void Interact();
+}
+/// <summary>
+/// interface IBreakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// int 
+    /// </summary>
+    int durability { get; set; }
+    /// <summary>
+    /// method
+    /// </summary>
+    void Break();
+}
+/// <summary>
+/// interface ICollectable
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// property
+    /// </summary>
+    bool isCollected { get; set; }
+    /// <summary>
+    /// method
+    /// </summary>
+    void Collect();
+}
+/// <summary>
+/// obstract class
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// string name
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// Overide ToString Method
+    /// </summary>
+    public override string ToString()
+    {
+        return ($"{name} is a {this.GetType()}");
+    }
+}
+
+public class Door : Base, IInteractive
+{
+    public string name { get; set; }
+
+
+    public Door(string name)
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
