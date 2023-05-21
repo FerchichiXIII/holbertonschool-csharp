8d4ab151abf2bd1f8c8ad4a21066a676cf666da8
FerchichiXIII <anasferchichi22@gmail.com>
2023-05-21 15:38:30 +0100

    --fix this

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -4,7 +4,7 @@
 /// Class Base
 /// </summary>
 
-abstract class Base
+public abstract class Base
 {
     /// <summary>
     /// The name of the object.
@@ -17,37 +17,94 @@ abstract class Base
     }
 }
 
-public interface IInteractive
-{
+
+/// <summary>
+/// Ineterface Interactive
+/// </summary>
+public interface IInteractive {
+    /// <summary>
+    /// Intercat method
+    /// </summary>
     void Interact();
 }
 
-public interface IBreakable
-{
-    int durability { get; set; }
-    void Break();
+/// <summary>
+/// Interface Breakable
+/// </summary>
+public interface IBreakable {
+    /// <summary>
+    /// durability integer
+    /// </summary>
+    int durability {
+        get;
+        set;
+    }
+    /// <summary>
+    /// break method
+    /// </summary>
+    void Break();   
 }
 
-public interface ICollectable
-{
-    bool isCollected { get; set; }
+/// <summary>
+/// Interface Collectable
+/// </summary>
+public interface ICollectable {
+    /// <summary>
+    /// isCOllected property
+    /// </summary>
+
+    bool isCollected {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Collect method
+    /// </summary>
     void Collect();
 }
 
-class TestObject : Base, IInteractive, IBreakable, ICollectable
-{
-    public int durability { get; set; }
-    public bool isCollected { get; set; }
+/// <summary>
+/// Test object class
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable {
 
+    /// <summary>
+    /// durability property
+    /// </summary>
+
+    public int durability {
+        get;
+        set;
+    }
+    /// <summary>
+    /// isCollected
+    /// </summary>
+    public bool isCollected {
+        get;
+        set;
+    }
+
+    /// <summary>
+    /// Interact inmplementation
+    /// </summary>
     public void Interact()
     {
+
     }
 
+    /// <summary>
+    /// Break implementation
+    /// </summary>
     public void Break()
     {
+
     }
 
+    /// <summary>
+    /// COllect implementation
+    /// </summary>
     public void Collect()
     {
+
     }
 }
