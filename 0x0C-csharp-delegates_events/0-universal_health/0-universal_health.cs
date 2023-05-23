71a97c6aa37a3dc088cc71771b4f7ee5ce3442c7
FerchichiXIII <anasferchichi22@gmail.com>
2023-05-23 21:16:57 +0100

    Universal health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f )
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.name = name;
+            this.maxHp = 100f;
+        }
+
+        this.hp = this.maxHp;
+        this.maxHp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}   
