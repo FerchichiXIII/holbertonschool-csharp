f29eaf466bcae7d13cbfa5e5fbd8cb9f361933d6
FerchichiXIII <anasferchichi22@gmail.com>
2023-06-08 23:38:26 +0100

    fix xml

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,32 +1,41 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a player with name, maximum health (maxHp), and current health (hp).
+/// </summary>
 public class Player
 {
     private string name;
     private float maxHp;
     private float hp;
 
-    public Player(string name = "Player", float maxHp = 100f )
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Player"/> class.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health of the player.</param>
+    public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
+
         if (maxHp > 0)
         {
-            this.name = name;
             this.maxHp = maxHp;
         }
         else
         {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-            this.name = name;
             this.maxHp = 100f;
         }
 
         this.hp = this.maxHp;
-        this.maxHp = this.maxHp;
     }
 
+    /// <summary>
+    /// Prints the current health of the player.
+    /// </summary>
     public void PrintHealth()
     {
         Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
-}   
+}
