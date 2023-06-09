f569fbae0bbd8218b6f20aad72e392fcfa86f198
FerchichiXIII <anasferchichi22@gmail.com>
2023-06-09 01:04:26 +0100

    Damage delegation

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+/// <summary>
+/// Represents a player with name, maximum health (maxHp), and current health (hp).
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Player"/> class.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health of the player.</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Prints the current health of the player.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    /// <summary>
+    /// Applies damage to the player.
+    /// </summary>
+    /// <param name="damage">The amount of damage to be applied.</param>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+
+        this.hp -= damage;
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+    }
+
+    /// <summary>
+    /// Heals the player.
+    /// </summary>
+    /// <param name="heal">The amount of health points to be healed.</param>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0;
+        }
+
+        this.hp += heal;
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+    }
+}
